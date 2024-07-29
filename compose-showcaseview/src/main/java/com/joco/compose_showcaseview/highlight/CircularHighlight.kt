c6a8be8f487c734a4afc10fb63bce4a3614f1c63
jocoand <joco.learn@gmail.com>
2024-07-29 17:44:45 +0700

    Create CircularHighlight

---
+++ compose-showcaseview/src/main/java/com/joco/compose_showcaseview/highlight/CircularHighlight.kt
@@ -0,0 +1,25 @@
+package com.joco.compose_showcaseview.highlight
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.boundsInRoot
+import com.joco.compose_showcaseview.highlightPaddingPx
+
+fun DrawScope.drawCircularHighlight(
+    coordinates: LayoutCoordinates
+) {
+    val targetRect = coordinates.boundsInRoot()
+    val xOffset = targetRect.topLeft.x
+    val yOffset = targetRect.topLeft.y
+    val rectSize = coordinates.boundsInParent().size
+    drawCircle(
+        color = Color.White,
+        radius = (rectSize.width / 2) + highlightPaddingPx,
+        center = Offset(xOffset + rectSize.width / 2, yOffset + rectSize.height / 2),
+        blendMode = BlendMode.Clear
+    )
+}
