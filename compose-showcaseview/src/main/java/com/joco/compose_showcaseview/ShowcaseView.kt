ee55d979e56f07acbc100f0f831576c106efeeae
jocoand <joco.learn@gmail.com>
2024-07-29 17:47:21 +0700

    Implement drawHighlight

--- compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseView.kt
+++ compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseView.kt
@@ -16,20 +16,18 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.LayoutCoordinates
-import androidx.compose.ui.layout.boundsInParent
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import com.joco.compose_showcaseview.highlight.drawRoundedRectHighlight
 
 /**
  * A Composable function that displays a dialog with a background overlay.
@@ -41,6 +39,7 @@ import androidx.compose.ui.unit.dp
  * @param alignment The alignment of the dialog relative to the target element.
  * @param duration The duration of the fade in and fade out animation.
  * @param onDisplayStateChanged: A callback function that is invoked when the display state of the Showcase changes.
+ * @param drawHighlight Draws the highlight around the target element.
  * @param dialog The content of the dialog.
  */
 @Composable
@@ -51,6 +50,7 @@ fun ShowcaseView(
     alignment: ShowcaseAlignment = ShowcaseAlignment.Default,
     duration: ShowcaseDuration = ShowcaseDuration.Default,
     onDisplayStateChanged: (ShowcaseDisplayState) -> Unit = {},
+    drawHighlight: DrawScope.(LayoutCoordinates) -> Unit = { drawRoundedRectHighlight(it, 8.dp.toPx()) },
     dialog: @Composable () -> Unit
 ) {
     val transition =  remember { MutableTransitionState(false) }
@@ -60,7 +60,10 @@ fun ShowcaseView(
         exit = fadeOut(tween(duration.exitMillis))
     ) {
         Box {
-            ShowcaseBackground(targetCoordinates)
+            ShowcaseBackground(
+                coordinates = targetCoordinates,
+                drawHighlight = { drawHighlight(targetCoordinates) }
+            )
             ShowcaseDialog(
                 targetRect = targetCoordinates.boundsInRoot(),
                 position = position,
@@ -87,15 +90,13 @@ fun ShowcaseView(
  * A Composable function that draws the background overlay and the highlight around the target element.
  *
  * @param coordinates The coordinates of the target element that the Showcase is highlighting.
+ * @param drawHighlight draws the highlight around the target element.
  */
 @Composable
-private fun ShowcaseBackground(coordinates: LayoutCoordinates) {
-    val targetRect = coordinates.boundsInRoot()
-    val xOffset = targetRect.topLeft.x
-    val yOffset = targetRect.topLeft.y
-    val rectSize = coordinates.boundsInParent().size
-    val cornerRadius = with(LocalDensity.current) { 8.dp.toPx() }
-
+private fun ShowcaseBackground(
+    coordinates: LayoutCoordinates,
+    drawHighlight: DrawScope.(LayoutCoordinates) -> Unit
+) {
     Canvas(
         modifier = Modifier
             .fillMaxSize()
@@ -106,17 +107,7 @@ private fun ShowcaseBackground(coordinates: LayoutCoordinates) {
             Color.Black.copy(alpha = 0.6f),
             size = Size(size.width, size.height)
         )
-        // Highlight
-        drawRoundRect(
-            color = Color.White,
-            size = Size(
-                rectSize.width + (highlightPaddingPx * 2),
-                rectSize.height + (highlightPaddingPx * 2)
-            ),
-            topLeft = Offset(xOffset - highlightPaddingPx, yOffset - highlightPaddingPx),
-            blendMode = BlendMode.Clear,
-            cornerRadius = CornerRadius(cornerRadius, cornerRadius)
-        )
+        drawHighlight(coordinates)
     }
 }
 
