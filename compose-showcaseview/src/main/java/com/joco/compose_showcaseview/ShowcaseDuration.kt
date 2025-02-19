1d6d056022046ffef2ebef7ec8d8336c196720fd
jocoand <joco.learn@gmail.com>
2025-02-20 01:05:17 +0700

    Cleanup

--- compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
+++ compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
@@ -1,9 +1,9 @@
 package com.joco.compose_showcaseview
 
 /**
- * Class that represents the duration of  animations in the `ShowcaseView`.
+ * Class that represents the duration of animations in the `ShowcaseView`.
  *
- * @property enterMillis The duration of the  animation in milliseconds.
+ * @property enterMillis The duration of the animation in milliseconds.
  * @property exitMillis The duration of the animation in milliseconds.
  */
 class ShowcaseDuration(val enterMillis: Int, val exitMillis: Int) {
