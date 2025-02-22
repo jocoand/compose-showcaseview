dd27e43822b0a3676b7577e7fb54351fb65c940d
jocoand <joco.learn@gmail.com>
2025-02-22 09:07:29 +0700

    Provide secondary constructor

--- compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
+++ compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
@@ -7,6 +7,14 @@ package com.joco.compose_showcaseview
  * @property exitMillis The duration of the animation in milliseconds.
  */
 class ShowcaseDuration(val enterMillis: Int, val exitMillis: Int) {
+
+    /**
+     * Constructor that sets the same duration for both enter and exit animations.
+     *
+     * @param durationMillis The duration of the animations in milliseconds.
+     */
+    constructor(durationMillis: Int) : this(durationMillis, durationMillis)
+
     companion object {
         private const val DEFAULT_MILLIS = 700
 
