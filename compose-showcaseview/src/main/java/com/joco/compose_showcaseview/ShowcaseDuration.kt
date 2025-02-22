32a7d9996ae45b48a2a8e16c7ceb899e418910c1
jocoand <joco.learn@gmail.com>
2025-02-22 12:01:58 +0700

    Refactor use factory for creation

--- compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
+++ compose-showcaseview/src/main/java/com/joco/compose_showcaseview/ShowcaseDuration.kt
@@ -6,11 +6,26 @@ package com.joco.compose_showcaseview
  * @property enterMillis The duration of the animation in milliseconds.
  * @property exitMillis The duration of the animation in milliseconds.
  */
-data class ShowcaseDuration(val enterMillis: Int, val exitMillis: Int) {
+class ShowcaseDuration private constructor(val enterMillis: Int, val exitMillis: Int) {
 
     companion object {
         private const val DEFAULT_MILLIS = 700
 
         val Default = ShowcaseDuration(DEFAULT_MILLIS, DEFAULT_MILLIS)
+
+        /**
+         * Creates a `ShowcaseDuration` with the same duration for both enter and exit animations.
+         *
+         * @param durationMillis Animations duration in milliseconds.
+         */
+        fun create(durationMillis: Int) = ShowcaseDuration(durationMillis, durationMillis)
+
+        /**
+         * Creates a `ShowcaseDuration` with different durations for enter and exit animations.
+         *
+         * @param enterMillis The enter animation duration in milliseconds.
+         * @param exitMillis The exit animation duration in milliseconds.
+         */
+        fun create(enterMillis: Int, exitMillis: Int) = ShowcaseDuration(enterMillis, exitMillis)
     }
 }
